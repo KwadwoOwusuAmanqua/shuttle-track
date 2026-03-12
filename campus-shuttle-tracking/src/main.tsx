45b0a727bcd8c636af212876f55f324bdad1be5a
Bavaria <BAVARIANLIVES@GMAIL.COM>
2026-03-12 19:19:04 +0000

    added auth pages

--- campus-shuttle-tracking/src/main.tsx
+++ campus-shuttle-tracking/src/main.tsx
@@ -1,12 +1,8 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import AppRouter from "./router/AppRouter.tsx";
 import "./styles/style.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <AppRouter />
-  </StrictMode>,
 );
--- campus-shuttle-tracking/src/router/AppRouter.tsx
+++ campus-shuttle-tracking/src/router/AppRouter.tsx
@@ -1,21 +1,26 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StudentLayout from "../layouts/StudentLayout";
 import MapPage from "../pages/student/Map";
+import { SignUp } from "../pages/auth/SignUp";
+import Login from "../pages/auth/Login";
+
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+
         {/* Student Routes */}
         <Route path="/" element={<StudentLayout />}>
-          <Route path="/" element={<Navigate to="/map" replace />} />
-          <Route path="map" element={<MapPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<SignUp />} />
         </Route>
 
         {/* Admin Routes */}
 
         {/* Fallback Route */}
-        <Route path="*" element={<Navigate to="/map" replace />} />
+
       </Routes>
     </BrowserRouter>
   );
